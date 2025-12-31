ac2b029c0e989a9374b49fde4dd4db0664249a6c
charancherry567367-ux <charancherry567367@gmail.com>
2025-12-31 11:58:32 +0530

    Create task4.java

---
+++ module1_array/task4.java
@@ -0,0 +1,17 @@
+code
+  class Main {
+    
+    static int AddTwo(int a, int b){
+        int c = a+b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int result = AddTwo(a,b);
+        
+        
+        System.out.println("Sum = " + result);
+    }
+}
